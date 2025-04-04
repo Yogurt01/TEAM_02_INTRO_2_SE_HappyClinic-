f4e1c11efebce04e7e47f07ebd43643e3eec8a2b
22120010 <hoangtuananh2004.work@gmail.com>
2025-04-04 08:07:10 +0700

    adjust default routes to getRegister

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db')
+const authController = require('./controllers/authController')
 require('dotenv')
 const app = express();
 connectDB()
+app.use('/',authController.getRegister)
+app.use('/auth', authRoutes);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
