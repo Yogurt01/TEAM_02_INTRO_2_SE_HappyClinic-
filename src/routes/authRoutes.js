968ca8d488cc7ed9c6446941e24afd75e09e8e46
22120010 <hoangtuananh2004.work@gmail.com>
2025-05-30 18:31:13 +0700

    fixes(dashboard) changes dashboard routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,20 @@ const authController = require('../controllers/authController');
 const authenticateToken = require('../middlewares/authMiddlesware');
 const otp = require('../controllers/otpController')
 const router = express.Router();
+const passport = require("passport");
+
+
+
+router.get("/google", 
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get("/google/callback", 
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 
@@ -11,7 +25,6 @@ router.post('/register', authController.postRegister);
 
 router.get('/logout', authController.logout);
 
-router.get('/dashboard', authenticateToken);
 // OTP Confirmation
 router.post('/verify-otp', otp.verifyOTP);
 module.exports = router;
