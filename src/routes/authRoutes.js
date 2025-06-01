b781bb66bdb22acc3cddb0e34d5320f107361c3f
SinhThach <thachsinh96@gmail.com>
2025-06-01 21:53:41 +0700

    Thêm chức năng đăng ký lịch khám

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const authenticateToken = require('../middlewares/authMiddlesware');
 const otp = require('../controllers/otpController')
 const router = express.Router();
 const passport = require("passport");
+const availabilityController = require('../controllers/availabilityController');
 
 
 
@@ -27,4 +28,37 @@ router.get('/logout', authController.logout);
 
 // OTP Confirmation
 router.post('/verify-otp', otp.verifyOTP);
+
+router.get('/availability', authenticateToken, availabilityController.getAvailability);
+
+router.post('/availability', authenticateToken, availabilityController.postAvailability);
+
+router.post('/availability/check/:id', authenticateToken, async (req, res) => {
+  try {
+    await availabilityController.markChecked(req.params.id);
+    res.redirect('/auth/availability');
+  } catch (err) {
+    res.status(500).send("Error marking slot as checked");
+  }
+});
+
+router.post('/availability/delete/:id', authenticateToken, async (req, res) => {
+  try {
+    await availabilityController.deleteAvailability(req.params.id);
+    res.redirect('/auth/availability');
+  } catch (err) {
+    res.status(500).send("Error deleting slot");
+  }
+});
+
+router.post('/availability/edit/:id', authenticateToken, async (req, res) => {
+  try {
+    await availabilityController.editAvailability(req.params.id, req.body);
+    res.redirect('/auth/availability?filterStatus=all')
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error editing slot");
+  }
+});
+
 module.exports = router;
