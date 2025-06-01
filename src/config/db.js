4d86e9d1adf057308aa51392757c4b586ec3e8de
yogurt01 <nguyenhuuben2709@gmail.com>
2025-06-01 15:55:32 +0700

    Thêm tính năng đặt lịch hẹn (appointment booking)

--- src/config/db.js
+++ src/config/db.js
@@ -3,16 +3,31 @@ require("dotenv").config()
 
 const URI = process.env.MONGO_URI
 const connectDB = async () => {
+  // try {
+  //   // await mongoose.connect(URI, {
+  //   //   useNewUrlParser: true,
+  //   //   useUnifiedTopology: true,
+  //   // });
+  //   await mongoose.connect(URI).then(() => {
+  //     console.log('MongoDB connected successfully'))
+  //   .catch((err) => {
+  //     console.error('Error connecting to MongoDB:', err);
+  //     process.exit(1); // Exit process with failure
+  //   }
+  //   // console.log('MongoDB connected successfully');
+  // } catch (err) {
+  //   console.error('Error connecting to MongoDB:', err);
+  //   process.exit(1); // Exit process with failure
+  // }
   try {
-    await mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected successfully');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit process with failure
-  }
+  // Kết nối MongoDB sử dụng async/await
+  await mongoose.connect(URI);
+  console.log('MongoDB connected successfully');
+} catch (err) {
+  // Xử lý lỗi nếu kết nối không thành công
+  console.error('Error connecting to MongoDB:', err);
+  process.exit(1); // Thoát quá trình với lỗi
+}
 };
 
 module.exports = connectDB;
